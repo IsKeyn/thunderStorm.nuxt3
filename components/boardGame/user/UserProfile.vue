aed0f0b461bc813faafd8138542961c1e73bb368
Keyn <keyn-artur@yandex.ru>
2025-04-29 23:04:38 +0500

    Фикс предметов у игрока, разделение на использованные и не использованные

--- components/boardGame/user/UserProfile.vue
+++ components/boardGame/user/UserProfile.vue
@@ -13,17 +13,33 @@ const props = defineProps({
 
 <template>
 	<div v-if="userInfo">
-		<h2 v-if="userInfo.player_info" class="inv-title">Предметы игрока {{ userInfo.player_info.user.name }}</h2>
-		<span v-if="userInfo.inventory && userInfo.inventory.length === 0">Предметов нет</span>
-		<div
-				v-for="(element, key) in userInfo.inventory"
-				class="wrapper"
-				:key="key"
-		>
-			<ItemCard
-					:element="element.item"
-					@setOpenedImage="emit('setOpenedImage', $event)"
-			/>
+		<div class="mb-[2rem]">
+			<h2 v-if="userInfo.player_info" class="inv-title">Предметы игрока {{ userInfo.player_info.user.name }}</h2>
+			<span v-if="userInfo.inventory && userInfo.inventory.filter(item => !item.has_used).length === 0">Предметов нет</span>
+			<div
+					v-for="(element, key) in userInfo.inventory.filter(item => !item.has_used)"
+					class="wrapper"
+					:key="key"
+			>
+				<ItemCard
+						:element="element.item"
+						@setOpenedImage="emit('setOpenedImage', $event)"
+				/>
+			</div>
+		</div>
+		<div>
+			<h2 v-if="userInfo.player_info" class="inv-title">Использованные предметы игрока {{ userInfo.player_info.user.name }}</h2>
+			<span v-if="userInfo.inventory && userInfo.inventory.filter(item => item.has_used).length === 0">Предметов нет</span>
+			<div
+					v-for="(element, key) in userInfo.inventory.filter(item => item.has_used)"
+					class="wrapper"
+					:key="key"
+			>
+				<ItemCard
+						:element="element.item"
+						@setOpenedImage="emit('setOpenedImage', $event)"
+				/>
+			</div>
 		</div>
 	</div>
 </template>
