fa218d97bb261908e0b6b3b9b68aacd54e7f00af
Keyn <keyn-artur@yandex.ru>
2025-08-28 01:55:57 +0500

    - Добавлены страницы связки настолькой игры

---
+++ pages/admin/board-game-item-bind/[slug].vue
@@ -0,0 +1,122 @@
+<script setup>
+definePageMeta({
+	layout: 'admin',
+});
+
+import BreadCrumbs from '@/components/menu/BreadCrumbs.vue';
+import CreateEditForm from '@/components/admin/forms/CreateEditForm.vue';
+
+const form = ref(
+		{
+			item_id: {
+				name: 'ID предмета',
+				value: '',
+				type: 'text',
+				validateRules: 'required, minLength_3, maxLength_255',
+				classes: ['w-full', 'mt-[5px]'],
+			},
+			name: {
+				name: 'Название',
+				value: '',
+				type: 'text',
+				validateRules: 'required, minLength_3, maxLength_255',
+				classes: ['w-full', 'mt-[5px]'],
+			},
+			slug: {
+				name: 'Slug',
+				value: '',
+				type: 'text',
+				validateRules: 'required, minLength_3, maxLength_255',
+				classes: ['w-full', 'mt-[5px]'],
+				autoFill: {
+					sourceFieldKey: 'name',
+					rule: 'slug',
+				},
+			},
+			description: {
+				name: 'Описание',
+				value: '',
+				type: 'textarea',
+				validateRules: null,
+				classes: ['w-full', 'mt-[5px]', 'resize-y', 'min-h-[400px]'],
+			},
+			actions: {
+				name: 'Действия JSON',
+				value: '',
+				type: 'textarea',
+				validateRules: null,
+				classes: ['w-full', 'mt-[5px]', 'resize-y', 'min-h-[400px]'],
+			},
+			board_game_id: {
+				name: 'ID настолькой игры',
+				value: '',
+				type: 'text',
+				validateRules: 'required, maxLength_255',
+				classes: ['w-full', 'mt-[5px]'],
+			},
+			type: {
+				name: 'Тип',
+				value: '',
+				type: 'text',
+				validateRules: 'maxLength_255',
+				classes: ['w-full', 'mt-[5px]'],
+			},
+			image: {
+				name: 'Медиа',
+				value: '',
+				keyValueFromObject: 'id',
+				objectValue: null,
+				type: 'fileFromGallery',
+				validateRules: '',
+				classes: ['w-full', 'mt-[5px]'],
+			},
+			active: {
+				name: 'Активность',
+				value: '',
+				type: 'text',
+				validateRules: '',
+				classes: ['w-full', 'mt-[5px]'],
+			},
+		}
+);
+
+const pageType = ref('');
+const route = useRoute();
+
+const breadCrumbsArray = computed(() => {
+	const splitedPath = route.path.split('/');
+
+	if (Number.isInteger(Number(route.params.slug))) {
+		pageType.value = 'update';
+	} else if (route.params.slug === 'create') {
+		pageType.value = 'create';
+	}
+
+	return [
+		{
+			name: 'Админ панель',
+			href: `/${splitedPath[1]}`,
+		},
+		{
+			name: 'Предметы в настолке',
+			href: `/${splitedPath[1]}/${splitedPath[2]}`,
+		},
+		{
+			name: pageType.value === 'create' ? 'Создание' : 'Редактирование',
+			href: `/${splitedPath[1]}/${splitedPath[2]}/${splitedPath[3]}`,
+		},
+	];
+});
+</script>
+
+<template>
+	<div>
+		<BreadCrumbs :breadCrumbs="breadCrumbsArray" />
+		<CreateEditForm
+				:form="form"
+				:showAdditionalData="false"
+				:hasResource="true"
+				fetchUrl="admin/BoardGame/BoardGameItem"
+		/>
+	</div>
+</template>
---
+++ pages/admin/board-game-item-bind/index.vue
@@ -0,0 +1,73 @@
+<script setup>
+definePageMeta({
+	layout: 'admin',
+});
+
+import BreadCrumbs from '@/components/menu/BreadCrumbs.vue';
+import ListTable from '@/components/admin/list/ListTable.vue';
+
+const titles = ref(
+		{
+			id: {
+				name: 'id',
+			},
+			item_id: {
+				name: 'id предмета',
+			},
+			name: {
+				name: 'Название',
+			},
+			slug: {
+				name: 'Slug',
+			},
+			description: {
+				name: 'Описание',
+			},
+			actions: {
+				name: 'Действия (json)',
+			},
+			type: {
+				name: 'Тип',
+			},
+			board_game_id: {
+				name: 'ID настолькой игры',
+			},
+			active: {
+				name: 'Активность',
+			},
+		}
+);
+
+const pageType = ref('');
+const route = useRoute();
+
+const breadCrumbsArray = computed(() => {
+	const splitedPath = route.path.split('/');
+
+	if (Number.isInteger(Number(route.params.slug))) {
+		pageType.value = 'update';
+	} else if (route.params.slug === 'create') {
+		pageType.value = 'create';
+	}
+
+	return [
+		{
+			name: 'Админ панель',
+			href: `/${splitedPath[1]}`,
+		},
+		{
+			name: 'Предметы в настолке',
+			href: `/${splitedPath[1]}/${splitedPath[2]}`,
+		},
+	];
+});
+</script>
+
+<template>
+	<BreadCrumbs :breadCrumbs="breadCrumbsArray" />
+	<ListTable
+		:titles="titles"
+		titleKey="title"
+		fetchUrl="admin/BoardGame/BoardGameItem"
+	/>
+</template>
