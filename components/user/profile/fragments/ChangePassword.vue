cf4538fb2414ef1039725d7c7d871933ca71c4ac
Keyn <keyn-artur@yandex.ru>
2025-08-31 23:48:07 +0500

    Core - Редактор полей additional fields для профайла

--- components/user/profile/fragments/ChangePassword.vue
+++ components/user/profile/fragments/ChangePassword.vue
@@ -3,8 +3,10 @@ import ResetPasswordForm from '@/components/forms/ResetPasswordForm.vue';
 </script>
 
 <template>
-	<span class="title block mb-5">Изменение пароля</span>
-	<ResetPasswordForm />
+	<div class="mb-[1rem]">
+		<span class="title block mb-5">Изменение пароля</span>
+		<ResetPasswordForm />
+	</div>
 </template>
 
 <style lang="scss" scoped></style>
---
+++ components/user/profile/fragments/FieldSettings.vue
@@ -0,0 +1,156 @@
+<script setup>
+import AlertBox from '@/components/notifications/AlertBlock.vue';
+import FormGenerator from '@/components/forms/FormGenerator/FormGenerator.vue';
+import ActionButton from '@/components/layout/buttons/ActionButton.vue';
+
+import { onMounted } from "vue";
+
+import { useUserStore } from '@/stores/user';
+const userStore = useUserStore();
+
+import { validate } from '@/composables/validate.js';
+const { validateForm } = validate();
+
+import { api } from '@/composables/api.js';
+const { sendApiRequest, preparedRequestBody } = api();
+
+import { notifications } from '@/composables/notifications.js';
+const { alert, error } = notifications();
+
+import { useLoadStateStore } from '@/stores/loadState';
+const loadState = useLoadStateStore();
+
+const form = ref(
+		{
+			twitch: {
+				name: 'Ссылка Twitch канал',
+				value: '',
+				type: 'text',
+				placeholder: 'Twitch канал',
+				validateRules: 'required, minLength_2, maxLength_50',
+				classes: 'w-full',
+			},
+		},
+);
+
+onMounted(() => {
+	setValue();
+});
+
+const setValue = () => {
+	form.value.twitch.value = `https://www.twitch.tv/${twitch.value.value}`;
+}
+
+const twitch = computed(() => {
+	if (userStore && userStore.user && userStore.user.additional_fields) {
+		const twitchField = userStore.user.additional_fields.filter((item) => item.slug === 'twitch_channel');
+
+		if (twitchField.length > 0 && twitchField[0]) {
+			return twitchField[0];
+		}
+	}
+
+	return false;
+});
+
+const errorsMessages = ref([]);
+
+const sendForm = async () => {
+	for (const formKey in form.value) {
+		form.value[formKey].validateResult = '';
+	}
+
+	const { status, key, validateResult } = validateForm(form.value);
+
+	if (status) {
+		await sendRequest();
+	} else {
+		form.value[key].validateResult = validateResult;
+		errorsMessages.value = [validateResult];
+	}
+}
+
+const requestInProgress = ref(false);
+const requestName = 'changeProfileFields';
+
+const sendRequest = async () => {
+	requestInProgress.value = true;
+
+	try {
+		loadState.loadList[requestName] = {
+			name: requestName,
+			type: 'fetch',
+			preloaderType: null,
+			status: 'load',
+		};
+
+		const preparedFormData = preparedRequestBody(form.value);
+
+		const body = {
+			additional_fields: [
+				{
+					name: 'Twitch канал',
+					slug: 'twitch_channel',
+					value: preparedFormData.twitch,
+					sort: 100,
+				}
+			],
+		};
+
+		if (twitch.value) {
+			body.additional_fields.id = twitch.value.id;
+		}
+
+		const response = await sendApiRequest('auth/update-profile', 'POST', body);
+
+		if (response) {
+			requestInProgress.value = false;
+
+			if (loadState.loadList[requestName]) {
+				loadState.loadList[requestName].status = 'finish';
+			}
+			alert('Ссылка на Ваш twitch канал обновлена');
+		}
+	} catch (e) {
+		if (loadState.loadList[requestName]) {
+			loadState.loadList[requestName].status = 'error';
+		}
+
+		error(e);
+		requestInProgress.value = false;
+	}
+}
+</script>
+
+<template>
+	<div class="mb-[1rem]">
+		<span class="title block mb-5">Поля профиля</span>
+		<div v-if="userStore.user && Object.keys(userStore.user).length > 0">
+			<AlertBox
+					:errorsMessages="errorsMessages"
+					class="mb-2"
+			/>
+			<FormGenerator
+					v-for="(field, index) in form"
+					:key="index"
+					:name="index"
+					:element="field"
+					:showValidateError=true
+					validateErrorPosition="bottom"
+					:labelClasses="['block', 'mb-[10px]']"
+					:fieldClasses="field.classes"
+			/>
+			<ActionButton
+					buttonName="Обновить"
+					buttonClasses="btn btn-simple-1 min-w-24"
+					:actionInProgress="requestInProgress"
+					@startAction="sendForm()"
+			/>
+		</div>
+		<div v-else>
+			Данный функционал доступен только авторизованному пользователю
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped></style>
