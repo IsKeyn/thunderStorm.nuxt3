2103d4a99f3139d6574dcfe7a13026d5e41e1787
Keyn <keyn-artur@yandex.ru>
2026-02-06 20:03:59 +0500

    - Слайдер построенный на vue3-slider, используется для отображения обложек

---
+++ components/sliders/vueSlider/cards/Image.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { inject } from "vue";
+
+const layoutMethods = inject('layoutMethods');
+
+const props = defineProps({
+	item: {
+		type: Object,
+		required: true,
+	},
+});
+</script>
+
+<template>
+	<img
+			:src="item.src"
+			:alt="item.description"
+			class="cursor-pointer"
+			@click="layoutMethods.setOpenedImage(item)"
+	>
+</template>
